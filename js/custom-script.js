b8790271c64d8fb47119b3aad942130256d64166
Mobeen Abdullah <mobeenabdullah@gmail.com>
2015-09-04 06:10:38 -0700

    BeOnDemand V2
    New Pages Added

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,3 +1,9 @@
+/* bootstrap carousel */
 jQuery('.carousel').carousel({
 		interval: 7000
-	})
+})
+
+/* add check class on choose skills */
+$(".add-skill").click(function(){
+	$(this).toggleClass('checked');
+});

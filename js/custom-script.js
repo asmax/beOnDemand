4eeb81ed2deab7db8c225cc8bac8af2b4b3d68d7
Mobeen Abdullah <mobeenabdullah@gmail.com>
2015-09-10 05:15:14 +0500

    BeOnDemand V4
    New Pages Added

--- js/custom-script.js
+++ js/custom-script.js
@@ -7,3 +7,39 @@ jQuery('.carousel').carousel({
 $(".add-skill").click(function(){
 	$(this).toggleClass('checked');
 });
+
+$(function () {
+	
+    $('#datetimepicker12').datetimepicker({
+        inline: true,
+        sideBySide: true, 
+        format: 'DD/MM/YYYY'
+    });
+    $('#datetimepicker1, #datetimepicker2').datetimepicker({
+        format: 'DD/MM/YYYY'
+      /*  inline: true,
+        sideBySide: true*/
+
+    });
+    
+});
+
+
+
+// add class on load 
+$(function() {
+  $('#about').addClass('expand');
+});
+
+// price bar slider
+$("#ex6").slider();
+$("#ex6").on("slide", function(slideEvt) {
+    $("#ex6SliderVal").text(slideEvt.value);
+});
+
+$("#ex7").slider();
+$("#ex7").on("slide", function(slideEvt) {
+    $("#ex7SliderVal").text(slideEvt.value);
+});
+
+
